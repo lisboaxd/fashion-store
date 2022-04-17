8f214473b40e064efd77e1b695e9599911b8cdcf
Mateus Lisboa <mateuslnobre@gmail.com>
2022-04-17 17:10:14 -0300

    modify field querysets filtering just the user request resources

--- src/apps/sellers/admin.py
+++ src/apps/sellers/admin.py
@@ -8,10 +8,50 @@ class ImageInline(admin.TabularInline):
 
 
 class ProductAdmin(admin.ModelAdmin):
+
     inlines = [ImageInline]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(category__in=request.user.seller.categories.all())
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        if db_field.name == "category":
+            kwargs["queryset"] = Category.objects.filter(
+                pk__in=request.user.seller.categories.all().values_list("pk")
+            )
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(owner=request.user.seller)
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        if db_field.name == "owner":
+            kwargs["queryset"] = Seller.objects.filter(
+                pk=request.user.seller.pk
+            )
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class StockAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        categories = request.user.seller.categories.all()
+        return qs.filter(product__category__in=categories)
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        if db_field.name == "product":
+            categories = request.user.seller.categories.all()
+            kwargs["queryset"] = Product.objects.filter(
+                category__in=categories.values_list("pk", flat=True)
+            )
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 admin.site.register(Seller)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Stock)
+admin.site.register(Stock, StockAdmin)
